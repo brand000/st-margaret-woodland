1bf6051df34f25b407a87d68726964e53efa5421
Devon <d.william.leigh@gmail.com>
2022-10-17 12:06:37 -0300

    restructuring & initial dropdowns

--- src/js/main.js
+++ src/js/main.js
@@ -1,53 +1,50 @@
-
 //hamburger navbar
 function openNav() {
-  document.getElementById("nav-bar").style.width = '25%';
+  document.getElementById("nav-bar").style.width = "25%";
 }
 
 function closeNav() {
-  document.getElementById("nav-bar").style.width = '0%';
+  document.getElementById("nav-bar").style.width = "0%";
 }
 
 /* change menuBtn to menuBtn.open to open reactive navbar */
-const menuBtn = document.querySelector('.menu-btn');
-const navBar = document.querySelector('.nav-bar');
-const mainContent = document.querySelector('.main-content');
+const menuBtn = document.querySelector(".menu-btn");
+const navBar = document.querySelector(".nav-bar");
+const mainContent = document.querySelector(".main-content");
 let menuOpen = false;
-menuBtn.addEventListener('click', () => {
+menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
-      menuOpen = true;
-      menuBtn.classList.add('open');
-      mainContent.classList.add('open');
-      openNav();
-
+    menuOpen = true;
+    menuBtn.classList.add("open");
+    mainContent.classList.add("open");
+    openNav();
   } else {
-      menuOpen = false;
-      menuBtn.classList.remove('open');
-      navBar.classList.remove('open');
-      mainContent.classList.remove('open');
-      closeNav();
+    menuOpen = false;
+    menuBtn.classList.remove("open");
+    navBar.classList.remove("open");
+    mainContent.classList.remove("open");
+    closeNav();
   }
-})
+});
 
 //swiper init
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   loop: true,
 
   // pagination init
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
 
   // nav arrows init
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   // autoplay init
   autoplay: {
     delay: 5000,
   },
-
 });
