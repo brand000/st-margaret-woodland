d6031c0c48a611616d05bb7876228ad5442cb795
zilong2001 <101072068+zilong2001@users.noreply.github.com>
2022-11-04 15:11:48 -0300

    Update interactive map.js

--- pages/map/interactive map.js
+++ pages/map/interactive map.js
@@ -1,21 +1,29 @@
 function setup(){
-  document.getElementById("description").style.visibility = "hidden";
-  document.getElementById("detail_description").style.visibility = "hidden";
+  document.getElementById("description1").style.display = "none";
+  document.getElementById("detail_description").style.display = "none";;
   console.log("visible");
 }
 
 function visible(){
-  document.getElementById("description").style.visibility = "visible";
+  document.getElementById("description1").style.display = "inline";
   console.log("visible");
   }   
 
 function invisible(){
-  document.getElementById("description").style.visibility = "hidden";
+  document.getElementById("description1").style.display = "none";
   console.log("hidden");
 }
 
 
 function detail_block(){
-  document.getElementById("detail_description").style.visibility = "visible";
+  document.getElementById("detail_description").style.display = "inline";
 }
 
+
+function detect_Click(event){
+  if (event.target != document.getElementById("detail_description")){
+    document.getElementById("detail_description").style.display = "none";
+  } else{
+   
+  }
+}
