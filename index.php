d6d971c00660399c9d8ec3cc77f9475bfc0e7ab3
Devon <d.william.leigh@gmail.com>
2022-11-13 17:27:42 -0400

    nav fixes

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<link rel="stylesheet" href="styles/body.css?v=<?php echo time(); ?>">
-<link rel="stylesheet" href="styles/nav.css?v=<?php echo time(); ?>">
-<link rel="stylesheet" href="styles/header.css?v=<?php echo time(); ?>">
-<link rel="stylesheet" href="styles/footer.css?v=<?php echo time(); ?>">
-<link rel="stylesheet" href="styles/index.css?v=<?php echo time(); ?>">
+<link rel="stylesheet" href="/styles/body.css?v=<?php echo time(); ?>">
+<link rel="stylesheet" href="/styles/nav.css?v=<?php echo time(); ?>">
+<link rel="stylesheet" href="/styles/header.css?v=<?php echo time(); ?>">
+<link rel="stylesheet" href="/styles/footer.css?v=<?php echo time(); ?>">
+<link rel="stylesheet" href="/styles/index.css?v=<?php echo time(); ?>">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 <script defer src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
-<script defer src="scripts/main.js"></script>
+<script defer src="/scripts/main.js"></script>
 
 
 
