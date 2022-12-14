cdeedd2ab59c45f313e23446cfe8ab355c32209a
brand000 <branwatson7777@gmail.com>
2022-12-14 19:39:57 -0400

    docummenting

--- pages/burial/script/burial information.js
+++ pages/burial/script/burial information.js
@@ -4,13 +4,12 @@
 //   Brandon Watson - function, styling, content, structure
 
 
-
+// setup the page javascript 
 function setup(){
     document.getElementById("agreement").style.display = "none";
 }
 
 // show booking aggreement
-
 function clickable(){
     document.getElementById("agreement").style.display = "block";
     document.getElementById("information_container").style.pointerEvents = "none";
@@ -18,10 +17,12 @@ function clickable(){
     change_opacity();
 }
 
+// remove the scrol
 function no_scroll(){
-    document.body.style.position = "fixed";
+    ldocument.body.style.position = "fixed";
 }
 
+//controll the of the header
 function change_opacity(){
     const elements = Array.from(document.getElementsByClassName("change_part"));
     elements.forEach(box => {
@@ -30,18 +31,22 @@ function change_opacity(){
     document.getElementById("header").style.opacity = "0.9";
 }
 
+// change button color
 function color_change(){
     document.getElementById("button").style.backgroundColor = "grey";
 }
 
+// change button color
 function color_back(){
     document.getElementById("button").style.backgroundColor = "white";
 }
 
+// go back and refresh
 function go_back(){
     window.location.reload();
 }
 
+// link to burial page
 function next(){
     window.location.href = "burial.html";
 }
