5d9db7de7b06376688b3ce2147e0388fe9681636
RahabarMahmud <rahabarmahmud@gmail.com>
2022-12-14 22:20:05 -0400

    Update burial information.js

--- pages/burial/script/burial information.js
+++ pages/burial/script/burial information.js
@@ -10,13 +10,17 @@ before they continue. The agreement only occur when visitor click button.
 All functions here will relate to the effect after click "burial booking"  
 */
 
-/*This is a function. hide the document  */
+/*
+ *This is a function. hide the document 
+ */
 function setup(){
     document.getElementById("agreement").style.display = "none";
 }
 
 
-/* This is a function. show booking aggreement, at the same time, other places are unclickable */
+/*
+ * This is a function. show booking aggreement, at the same time, other places are unclickable 
+*/
 function clickable(){
     document.getElementById("agreement").style.display = "block";
     document.getElementById("information_container").style.pointerEvents = "none";
@@ -24,12 +28,16 @@ function clickable(){
     change_opacity();
 }
 
-/* This is a function. When aggreement occur, the body's scrollbar can not be used */
+/* 
+ *This is a function. When aggreement occur, the body's scrollbar can not be used 
+*/
 function no_scroll(){
     document.body.style.position = "fixed";
 }
 
-/*This is a function. When aggreement occur, the opacity change */
+/*
+*This is a function. When aggreement occur, the opacity change 
+*/
 function change_opacity(){
     const elements = Array.from(document.getElementsByClassName("change_part"));
     elements.forEach(box => {
@@ -41,24 +49,28 @@ function change_opacity(){
 
 
 /* This is a function. When users put their cursor on "burial booking" button,
- the button color will change to grey */
+ *the button color will change to grey 
+ */
 function color_change(){
     document.getElementById("button").style.backgroundColor = "grey";
 }
 
 
 /* This is a function. When usersmove their cursor out "burial booking" button,
- the button color will back to white */
+ *the button color will back to white 
+ */
 function color_back(){
     document.getElementById("button").style.backgroundColor = "white";
 }
 
-/* This is a function. refresh the page, when user click "back" button */
+/* This is a function. refresh the page, when user click "back" button 
+*/
 function go_back(){
     window.location.reload();
 }
 
-/* This is a function. redirct to burial booking page, when user click "I agree" button */
+/* This is a function. redirct to burial booking page, when user click "I agree" button 
+*/
 function next(){
     window.location.href = "burial.html";
 }
